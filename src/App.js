0f52c6fdd7e014d97c69831e3d2fb5bc6d3b5747
Vabalabadabdab <110040060+Deadusik@users.noreply.github.com>
2023-01-25 02:11:42 +0200

    finish full size footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import AppRouter from "./components/AppRouter";
+import AppRouter from "./components/AppRouter"
 import Navbar from './components/navbar/Navbar'
 import './styles/nullingstyle.scss'
 import './styles/fonts.scss'
 import {
   useLocation
 } from 'react-router-dom'
-import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer"
+import styles from './styles/app.module.scss'
 
 function App() {
   const location = useLocation();
 
   return (
-    <>
+    <div className={styles.App}>
       <Navbar
         pathname={location.pathname} />
       <AppRouter />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+import '../../styles/wrapping.scss'
+//imgs 
+import logoSrc from '../../imgs/logo.png'
+import whatsappSrc from '../../imgs/icons/social_media/whatsapp.png'
+import instagramSrc from '../../imgs/icons/social_media/instagram.png'
+import mailSrc from '../../imgs/icons/social_media/mail.png'
+import facebookSrc from '../../imgs/icons/social_media/facebook.png'
+import whatsappQRSrc from '../../imgs/whatsapp_qr.png'
 
 const Footer = () => {
     return (
         <footer className={styles.Footer}>
-            <div className={styles.Footer__Content}>
+            <div className={[styles.Footer__Content, 'wrapcontent'].join(' ')}>
                 { /* left column */}
                 <div className={styles.Title}>
-
+                    <img
+                        className={styles.Title__Logo}
+                        src={logoSrc}
+                        alt='logo' />
+                    <p>
+                        Ріелторська<br />
+                        агенція
+                    </p>
+                    <div className={styles.Title__Icons}>
+                        <a href='/'>
+                            <img className={styles.Title__Icon} src={whatsappSrc} alt='social media' />
+                        </a>
+                        <a href='/'>
+                            <img className={styles.Title__Icon} src={instagramSrc} alt='social media' />
+                        </a>
+                        <a href='/'>
+                            <img className={styles.Title__Icon} src={mailSrc} alt='social media' />
+                        </a>
+                        <a href='/'>
+                            <img className={styles.Title__Icon} src={facebookSrc} alt='social media' />
+                        </a>
+                    </div>
                 </div>
                 { /* center column */}
                 <div className={styles.Info}>
-
+                    <p>Контакти</p>
+                    <p>+380 73 102 01 20</p>
+                    <p>komfort.uman@gmail.com</p>
+                    <p>
+                        Години роботи: <br />
+                        Пн-Пт: 9.00-18.00
+                    </p>
                 </div>
                 { /* right column */}
                 <div className={styles.QRCode}>
-
+                    <img src={whatsappQRSrc} alt='qr code' />
+                    <p>Чат підтримки</p>
+                    <p>WhatsApp</p>
                 </div>
             </div>
         </footer>
